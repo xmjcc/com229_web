9389fb6ee5aa35aa583d49110b26202e8eb4be9d
xmjcc <benjaminwang2088@gmail.com>
2024-10-01 17:23:34 -0400

    check home page problem

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -25,7 +25,7 @@ const MainRouter = () => {
  <Layout/>
  <Routes>
  
- <Route exact path="/" element={<Home/>} />
+ <Route exact path="/home" element={<Home/>} />
  <Route exact path="/about" element={<About/>} />
  <Route exact path="/projects" element={<Projects/>} />
  <Route exact path="/services" element={<Services/>} />
